19affd365c72f60c6fd9db42b637c497fc86e64e
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-11 11:00:41 +0530

    fix PR comments

--- lib/animations/animated_container/animated_container.dart
+++ lib/animations/animated_container/animated_container.dart
@@ -16,24 +16,13 @@ class _AnimatedContainerScreenState extends State<AnimatedContainerScreen> {
   double _shapeWidth = 20; // initial width of shape
   double _shapeHeight = 20; // initial height of shape
   Color _color = Colors.greenAccent; // initial color of shape
-  BorderRadiusGeometry _shapeBorderRadius =
-      BorderRadius.circular(8); // initial border radius
+  late BorderRadiusGeometry _shapeBorderRadius; // initial border radius
 
-  // This function is used to start the animation
-  // It will be triggered when the button is pressed
-  void _start() {
-    setState(() {
-      //Using random generate values for height, width, colors and border radius that will be animate
-      final random = Random();
-      _shapeWidth = random.nextInt(1000).toDouble();
-      _shapeHeight = random.nextInt(1000).toDouble();
-      _color = Color.fromRGBO(
-          random.nextInt(256), random.nextInt(256), random.nextInt(256), 1);
-      _shapeBorderRadius =
-          BorderRadius.circular(random.nextInt(200).toDouble());
-    });
+  @override
+  void initState() {
+    super.initState();
+    _shapeBorderRadius = BorderRadius.circular(8);
   }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -44,25 +33,33 @@ class _AnimatedContainerScreenState extends State<AnimatedContainerScreen> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: AnimatedContainer(
-              //https://api.flutter.dev/flutter/widgets/AnimatedContainer-class.html
               duration: const Duration(seconds: 4), // Animation duration
               width: _shapeWidth,
               height: _shapeHeight,
               decoration: BoxDecoration(
                   borderRadius: _shapeBorderRadius, color: _color),
-              curve: Curves
-                  .bounceInOut, // https://api.flutter.dev/flutter/animation/Curves-class.html
+              curve: Curves.bounceInOut,
             ),
           ),
         ),
         FilledButton(
-            // start animation using button click
-            onPressed: _start, // call start method to begin animation
+            onPressed: () {
+              setState(() {
+                //Using random generate values for height, width, colors and border radius that will be animate
+                final random = Random();
+                _shapeWidth = random.nextInt(1000).toDouble();
+                _shapeHeight = random.nextInt(1000).toDouble();
+                _color = Color.fromRGBO(random.nextInt(256),
+                    random.nextInt(256), random.nextInt(256), 1);
+                _shapeBorderRadius =
+                    BorderRadius.circular(random.nextInt(200).toDouble());
+              });
+            }, // call start method to begin animation
             child: const Text('Start Animation')),
         const Positioned(
           left: 0,
           right: 0,
-          bottom: 12,
+          bottom: 32,
           height: 100,
           child: ContributorsCard(
             imageUrl:
